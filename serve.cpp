a4fcc2e624461be7a6c7ee3dc17ab398db327b26
Max Ushakov <ushmax@gmail.com>
2010-03-29 23:28:07 +0400

    start working on html search

--- serve.cpp
+++ serve.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <fastcgi++/request.hpp>
 #include <fastcgi++/manager.hpp>
 #include <glib.h>
@@ -19,22 +20,78 @@ public:
         out.write(buf, 4);
     }
 
+    void output_binary(const std::vector<uint32_t>& x,
+                       const std::vector<uint32_t>& y,
+                       const std::vector<std::string>& titles,
+                       int cont) {
+        int text_size = 0;
+        for (int i = 0; i < titles.size(); ++i) {
+            text_size += titles[i].size();
+        }
+
+        int content_length = 4 + 4 + x.size()*12 + text_size;
+        out << "Content-Type: application/octet-stream\r\n";
+        out << "Content-Length: " << content_length << "\r\n";
+        out << "\r\n";
+
+        if (cont == DiskIndex::kEOF) {
+            out_int_be(-1);
+        } else {
+            out_int_be(cont);
+        }
+        out_int_be(x.size());
+        for (int i = 0; i < x.size(); ++i) {
+            out_int_be(x[i]);
+            out_int_be(y[i]);
+            out_int_be(titles[i].size());
+            out.write(titles[i].c_str(), titles[i].size());
+        }
+    }
+
+    std::string quote(std::string q) {
+        std::string o;
+        for (int i = 0; i < q.size(); ++i) {
+            if (q[i] == '\\') {
+                o += "\\";
+            } else if (q[i] == '\'') {
+                o += "\'";
+            } else {
+                o += q[i];
+            }
+        }
+        return o;
+    }
+
+    void output_json(std::string query,
+                     const std::vector<uint32_t>& x,
+                     const std::vector<uint32_t>& y,
+                     const std::vector<std::string>& titles,
+                     int cont) {
+        out << "Content-Type: application/json\r\n";
+        out << "\r\n";
+        out << "{'cont':" << cont << ", ";
+        out << "'q': '" << quote(query) << "',";
+        out << "'data': [";
+        for (int i = 0; i < x.size(); ++i) {
+            out << "{ 'x': " << x[i] << ", ";
+            out << "'y': " << y[i] << ", ";
+            out << "'title': '" << quote(titles[i]) << "'}, ";
+        }
+        out << "]}";
+    }
+
     bool response()
     {
         int id;
+        std::string query;
         if (environment.requestVarGet("id", id)) {
             // Document dump requested
             out << "Content-Type: text/html; charset=utf-8\r\n\r\n";
             g_index.DumpData(id, out);
             return true;
-        } else {
-            std::string query;
+        } else if (environment.requestVarGet("q", query)) {
             int start;
             int num;
-            if (!environment.requestVarGet("q", query)) {
-                out << "Status: 400\r\n\r\n";
-                return true;
-            }
             // For some reason, requestVarGet does not convert + back to space
             for (int i = 0; i < query.size(); ++i) {
                 if (query[i] == '+') {
@@ -59,28 +116,34 @@ public:
             std::vector<std::string> titles;
             uint32_t cont = g_index.Search(query, &x, &y, &titles, num, start);
 
-            int text_size = 0;
-            for (int i = 0; i < titles.size(); ++i) {
-                text_size += titles[i].size();
-            }
-
-            int content_length = 4 + 4 + x.size()*12 + text_size;
-            out << "Content-Type: application/octet-stream\r\n";
-            out << "Content-Length: " << content_length << "\r\n";
-            out << "\r\n";
-
-            if (cont == DiskIndex::kEOF) {
-                out_int_be(-1);
+            std::string output;
+            if (environment.requestVarGet("output", output) &&
+                output == "json") {
+                output_json(query, x, y, titles, cont);
             } else {
-                out_int_be(cont);
+                output_binary(x, y, titles, cont);
             }
-            out_int_be(x.size());
-            for (int i = 0; i < x.size(); ++i) {
-                out_int_be(x[i]);
-                out_int_be(y[i]);
-                out_int_be(titles[i].size());
-                out.write(titles[i].c_str(), titles[i].size());
+            
+            return true;
+        } else {
+            // Serve static files
+            std::string name;
+            if (!environment.requestVarGet("name", name)) {
+                name = "index.html";
             }
+            if (name.find("..") != std::string::npos) {
+                out << "Status: 400 Bad request\r\n\r\n";
+                return true;
+            }
+            name = "/home/ushakov/devel/maps/remote-search/st/" + name;
+            std::ifstream ifs(name.c_str());
+            if (!ifs.is_open()) {
+                out << "Status: 404 Not found\r\n\r\n";
+                return true;
+            }
+            out << "Content-Type: text/html; charset=utf-8\r\n";
+            out << "\r\n";
+            out.dump(ifs);
             return true;
         }
         return true;
